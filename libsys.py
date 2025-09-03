de5d0922b1708e359191d683862176bc7f048012
treemik <calebambler@gmail.com>
2025-09-03 10:50:07 +0800

    add subparser for adding authors

--- libsys.py
+++ libsys.py
@@ -12,4 +12,10 @@ add_parser.add_argument("-t","--title",required=True, help="The title of the boo
 add_parser.add_argument("-a","--author",required=True, help="The author of the book")
 add_parser.add_argument("-p","--published",help="The published date of the book")
 add_parser.add_argument("--isbn", help="The ISBN of the book")
+# add subparser add author
+add_parser=subparsers.add_parser("add_author", help="Add an author")
+add_parser.add_argument("-f","--first",required=True, help="The first name of the author")
+add_parser.add_argument("-l","--last",required=True, help="The last name of the author")
+
+
 
