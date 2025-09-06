5d119ab94ea9ff3b603ee0ce4c4b1b8503879733
treemik <calebambler@gmail.com>
2025-09-06 10:00:29 +0800

    feat: add published_type for argparse to validate and normalize dates

--- libsys.py
+++ libsys.py
@@ -1,8 +1,21 @@
 from helper_functions import init_db, DatabaseContextManager
 from services import add_book
+import re
 from datetime import datetime
-import sqlite3
+
 import argparse
+#year regex
+year_re=re.compile(r"^\s*(\d{4})(?:-(\d{2})(?:-\d{2})?)?\s*$")
+def published_type(s: str)->int:
+    match = year_re.match(s)
+    if not match:
+        raise argparse.ArgumentTypeError(f"{s} is not a valid date please enter a vaild date (YYYY-MM-DD OR YYYY-MM OR YYYY)")
+    year = int(match.group(1))
+    now = int(datetime.now().year)
+    if not 1400 <= year <= now+1:
+        raise argparse.ArgumentTypeError(f"{year} is not a valid year")
+    return year
+
 
 #set up parser
 parser = argparse.ArgumentParser(description="A cli library system that adds books tracks loans and members")
@@ -12,7 +25,7 @@ subparsers.required = True
 add_parser = subparsers.add_parser("add_book", help="Add a book track")
 add_parser.add_argument("-t","--title",required=True, help="The title of the book")
 add_parser.add_argument("-a","--author",required=True, action="append", help="The author of the book")
-add_parser.add_argument("-p","--published",help="The published date of the book")
+add_parser.add_argument("-p","--published",type=published_type,help="The published date of the book")
 add_parser.add_argument("--isbn", help="The ISBN of the book")
 # add subparser add author
 add_parser=subparsers.add_parser("add_author", help="Add an author")
@@ -26,16 +39,16 @@ add_parser.add_argument("-e","--email",required=True,help="The email address of 
 add_parser.add_argument("-p","--phone",help="The phone number of the member")
 
 args = parser.parse_args()
-
+#Initilize the database
 init_db()
-
+#handle add book
 if args.command=="add_book":
     with DatabaseContextManager("library.db") as conn:
 
         if not args.published:
             pub_year=None
         else:
-            pub_year=int(args.published)
+            pub_year=(args.published)
         if not args.isbn:
             isbn=None
         else:
@@ -43,7 +56,7 @@ if args.command=="add_book":
         title = args.title
         authors = args.author
 
-        add_book(conn,title,authors,pub_year,isbn)
+        add_book(conn,title=title,authors=authors,pub_year=pub_year,isbn=isbn)
 
 
 
